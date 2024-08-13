6ea074312af3e014205615878b9309cf8ce8e3bb
Pedro Paulo <pedropaulo19977@gmail.com>
2024-08-13 20:00:41 -0300

    transformando funcao em api

--- index.mjs
+++ index.mjs
@@ -2,10 +2,13 @@ import { log } from './log.mjs';
 
 export const handler = async (event) => {
     log('event: ' + JSON.stringify(event));
-    
+
     return  {
         statusCode: 200,
-        body: JSON.stringify(event),
+        body: `<html><body>Dados da requisicao ${JSON.stringify(event)}</body></html>`,
+        headers: {
+            "content-type": "text/html"
+        }
       };
   };
   
